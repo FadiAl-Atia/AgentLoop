4587405b893101efd8485f8ffdc0454603414d0e
Fadi Al-Atia <fadi.alatia04@gmail.com>
2026-09-24 12:17:59 +0300

    Setup Input

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
 import Anthropic from "@anthropic-ai/sdk";
+import { input } from "@inquirer/prompts";
 
 const client = new Anthropic();
+
+const username = await input({ message: "What is your name?" });
+
+console.log("\nYour name is:  " + username);
